0934a2c653cec53e75d5db2f2c82cd89b82011b9
Myoungmin <miung222@gmail.com>
2022-08-30 19:56:40 +0900

    C++11 람다식, STL의 function 클래스를 사용하여 추상 인터페이스를 대체하는 실습
    // 추상 인터페이스와 추상 인터페이스 구현 클래스를 따로 만들지 않아도 된다.
    // 멤버 함수를 하나만 가지는 추상 인터페이스는 STL의 function 클래스를 사용해 간단하게 코드를 변환할 수 있다.
    // 클래스 결합 부분을 람다식이 담당하면, 특정 구현 수단과의 직접적인 결합을 피할 수 있다.
    
    // [&] 몸통에서 쓰이는 모든 변수나 상수를 참조로 캡처하고 현재 객체를 참조로 캡처.
     // 람다에서 이렇게 사용해야 graphics를 참조할 수 있다.

--- BindingClass.h
+++ BindingClass.h
@@ -5,8 +5,10 @@
 #include <cmath>
 
 #include <iostream>
+#include <functional>
 
 #include "Vector2.h"
+
 
 
 
@@ -67,7 +69,7 @@ public:
 // 그래픽 화면 전용
 class GraphicsRenderer : public Circle::Renderer {
 public:
-	GraphicsRenderer(Graphics& renderer) : renderer_(&renderer){
+	GraphicsRenderer(Graphics& renderer) : renderer_(&renderer) {
 
 	}
 
@@ -78,3 +80,57 @@ public:
 private:
 	Graphics* renderer_;	// 그래픽 화면 전용 렌더러
 };
+
+
+
+/************************************************************************************************************/
+
+/* C++11 람다식, STL의 function 클래스를 사용하여 추상 인터페이스를 대체하는 실습 */
+
+// 추상 인터페이스와 추상 인터페이스 구현 클래스를 따로 만들지 않아도 된다.
+// 멤버 함수를 하나만 가지는 추상 인터페이스는 STL의 function 클래스를 사용해 간단하게 코드를 변환할 수 있다.
+// 클래스 결합 부분을 람다식이 담당하면, 특정 구현 수단과의 직접적인 결합을 피할 수 있다.
+
+class LambdaCircle {
+public:
+	// 생성자
+	LambdaCircle(const Vector2& center, double radius) : center_(center), radius_(radius) {}
+
+	// 렌더링 (매개 변수로 람다식을 받는다!)
+	void draw(std::function<void(const Vector2&, double)> renderer) {
+		renderer(center_, radius_);
+	}
+
+	// 면적 계산
+	double area() const {
+		return radius_ * radius_ * M_PI;
+	}
+
+private:
+	Vector2 center_;	// 중심
+	double radius_;		// 반지름
+};
+
+// 람다식을 사용하는 형태 예시
+void Example()
+{
+	LambdaCircle lambdaCircle(Vector2(0, 0), 10);
+	Graphics graphics;
+
+	// 콘솔 화면에 출력
+	lambdaCircle.draw([](const Vector2& center, double radius) {
+		std::cout << "x:" << center.x << " y:" << center.y
+			<< " r:" << radius << std::endl;
+		}
+	);
+
+	// 그래픽 화면에 출력
+	// [&] 몸통에서 쓰이는 모든 변수나 상수를 참조로 캡처하고 현재 객체를 참조로 캡처.
+	// 람다에서 이렇게 사용해야 graphics를 참조할 수 있다.
+	lambdaCircle.draw([&](const Vector2& center, double radius) {
+		graphics.drawCircle(center, radius);
+		}
+	);
+}
+
+/************************************************************************************************************/
