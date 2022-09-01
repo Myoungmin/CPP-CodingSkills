9269494e6f7156ca5389b40a86f2f8ededd21470
Myoungmin <miung222@gmail.com>
2022-09-01 16:17:18 +0900

    case에 반드시 일치해야 하는 경우, default에 assert를 적어 부정확한 상태를 방지하는 실습
    // 부정확한 상태일 때 assert가 실행되어 아래와 같은 메시지가 표시된다.
    // Assertion failed: !"부정확한 상태", file CPP-CodingSkills.cpp, line 67
    // 문자열에 !(논리 부정 연산자)를 붙이면 true가 반전되어 false로 평가되어 assert가 오류라고 판단하게 된다.

--- CPP-CodingSkills.cpp
+++ CPP-CodingSkills.cpp
@@ -41,10 +41,45 @@ void WeekToNameAndNameToWeekExample()
     cout << to_String(to_week("Sun")) << endl;
 }
 
+
+
+/* case에 반드시 일치해야 하는 경우, default에 assert를 적어 부정확한 상태를 방지하는 실습 */
+
+void SwitchDefaultAssert()
+{
+    enum STATE {
+        STATE_RUN,
+        STATE_JUMP,
+        STATE_DAMAGE,
+        STATE_ERROR
+    };
+
+    STATE state = STATE_ERROR;
+
+    switch (state) {
+    case STATE_RUN:
+        // RUN 로직 생략
+        break;
+    case STATE_JUMP:
+        // JUMP 로직 생략
+        break;
+    case STATE_DAMAGE:
+        // DAMAGE 로직 생략
+        break;
+    default:
+        // 부정확한 상태일 때 assert가 실행되어 아래와 같은 메시지가 표시된다.
+        // Assertion failed: !"부정확한 상태", file CPP-CodingSkills.cpp, line 67
+        // 문자열에 !(논리 부정 연산자)를 붙이면 true가 반전되어 false로 평가되어 assert가 오류라고 판단하게 된다.
+        assert(!"부정확한 상태");
+        break;
+    }
+}
+
 int main()
 {
     //ClampExample();
     //WrapExample();
     //JudgementExample();
-    WeekToNameAndNameToWeekExample();
+    //WeekToNameAndNameToWeekExample();
+    SwitchDefaultAssert();
 }
